f3045b8d9dc3bedee3c9e7eaf16b34d8dbe70726
Kitasio <kit.plotits@gmail.com>
2025-07-01 16:44:40 +0200

    Fix cart icon

--- components/CartIcon.tsx
+++ components/CartIcon.tsx
@@ -19,7 +19,7 @@ export default function CartIcon() {
     >
       <Image alt="cart-icon" width={40} height={40} src="https://flurs.fly.storage.tigris.dev/web/cart.svg"/>
       {isCartReady && itemCount > 0 && (
-        <span className="absolute -top-1 -right-1 bg-gray-900 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full pointer-events-none">
+        <span className="absolute -top-1 -right-1 bg-lime-950 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full pointer-events-none">
           {itemCount}
         </span>
       )}
