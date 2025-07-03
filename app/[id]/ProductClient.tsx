a28f8fb6767169b8a3ad3014959dba2a042165a5
Kitasio <kit.plotits@gmail.com>
2025-07-03 20:45:56 +0200

    Change the style of the button "Add to cart"

--- app/[id]/ProductClient.tsx
+++ app/[id]/ProductClient.tsx
@@ -181,7 +181,7 @@ export default function ProductClient ({ poster }: ProductClientProps) {
         <button
           type="button"
           onClick={handleAddToCart}
-          className="text-green-700 hover:text-white transition duration-300 cursor-pointer border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
+         className="w-full text-[#404D30] hover:text-white transition duration-300 cursor-pointer border-1 border-[#404D30] hover:bg-[#404D30] focus:ring-4 focus:outline-none focus:ring-[#404D30] font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
         >
           ADD TO CART
         </button>
